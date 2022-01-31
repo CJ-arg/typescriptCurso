a6d7843e0212eeb496ee5ff506431cf8e53414a0
unknown <janoncarlos@gmail.com>
2022-01-31 15:48:16 -0300

    ejemplo05

---
+++ CursoLargo/ejemplo05.ts
@@ -0,0 +1,53 @@
+//Arrays numeros
+let numeros=[1,4,5,7];
+console.log(numeros);
+//Arrays strings
+let cadena5=['Hola','me','llama','Juan'];
+console.log(cadena);
+
+let cadena5:string[];
+cadena5=['Hola','me','llama','Pepe'];
+console.log(cadena5);
+
+let mezcla:any[];
+mezcla=['Hola','me','llama','Pepe',10,true];
+console.log(mezcla);
+
+let mezcla2=['Hola','me','llama','Pepito',11,true];
+console.log(mezcla2);
+
+let casa5=
+{
+    calle:"Calle melaconlia",
+    numero:13,
+    portero:false,
+    familia:
+    {
+        padre:"Juan",
+        hijo:"Pepe"
+    },
+    presentacion:["Hola","soy","el","hijo"]
+}
+
+let casa52=
+{
+    calle:"Calle melaconlia 2",
+    numero:13,
+    portero:false,
+    familia:
+    {
+        padre:"Juan 2",
+        hijo:"Pepe 2 "
+    },
+    presentacion:["Hola","soy","el","padre"]
+}
+
+console.log(casa5;
+
+let mezcla3=['Hola','me','llama','Pepito',11,true,cassa5];
+console.log(mezcla3);
+
+let casas:any[];
+casas=[casa,casa52]
+
+console.log(casas);
