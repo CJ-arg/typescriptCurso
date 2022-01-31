feeb5d32f688f27395c413bd1a0b8fb02ee33b48
unknown <janoncarlos@gmail.com>
2022-01-31 13:29:45 -0300

    ejemplo04

---
+++ CursoLargo/ejemplo03.ts
@@ -0,0 +1,5 @@
+let casa:string
+// asignacion de tipo 
+let casa2:"mi casa";
+// inferencia de tipo 
+casa =45
---
+++ CursoLargo/ejemplo04.ts
@@ -0,0 +1,46 @@
+//Tipos primitivos: string,number,boolean,null, undefined
+// Tipos no primitivos: el resto
+let casaGrande=
+{
+    calle:"Lavalle",
+    numero:13,
+    portero:false,
+    familia:
+    {
+        padre:"Juan",
+        hijo:"Pepe"
+    }
+}
+casaGrande.calle="34"
+console.log( typeof casaGrande.calle)
+/*console.log(casa.calle)
+console.log(casa.numero)
+console.log(casa.portero)*/
+
+let casaMediana:
+{
+    calle:string,
+    numero:number,
+    portero:boolean,
+    familia:
+    {
+        padre:string,
+        hijo:string
+    }
+}=
+{
+    calle:"Peru",
+    numero:45,
+    portero:true,
+    familia:
+    {        
+        padre:"Juan",
+        hijo:"Pepe"
+
+    }
+
+}
+
+console.log(casaMediana)
+/*casa2.calle ="alguna"
+console.log(casa2)*/
