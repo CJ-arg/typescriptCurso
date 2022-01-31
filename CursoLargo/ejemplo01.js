1da5089d5cecd34ae486cea1d74268616a12fa00
unknown <janoncarlos@gmail.com>
2022-01-31 12:54:19 -0300

    segundo curso 7 horas

--- CursoLargo/ejemplo01.js
+++
@@ -1,8 +0,0 @@
-function holaMundo() {
-    console.log("Hola Mundo");
-}
-function multiplicar(x, y) {
-    return x * y;
-}
-holaMundo();
-console.log(multiplicar(6, 8));
--- CursoLargo/ejemplo02.ts
+++ CursoLargo/ejemplo02.ts
@@ -1,9 +1,14 @@
-let numero2:Number;
-let cadena:String;
-let soyBoleano:Boolean;
+let numero2:number;
+let cadena:string;
+let soyBoleano:boolean;
 
 cadena="Hola Mundo"
 numero2=36
 soyBoleano=true
 
 console.log(cadena, numero2, soyBoleano);
+
+function Pruebas2(num:number,cad:number){
+return num + cad
+}
+console.log(Pruebas2(3,4));
