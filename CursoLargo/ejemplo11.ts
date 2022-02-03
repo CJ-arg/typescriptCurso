03193cb55ee1b047164ef4c9acb2dc9f62d088f6
unknown <janoncarlos@gmail.com>
2022-02-03 13:40:21 -0300

    leccion 11

---
+++ CursoLargo/ejemplo11.ts
@@ -0,0 +1,20 @@
+//FOR 1
+for(let i=0; i<=6; i++){
+   console.log("El número es " +i );
+}
+
+//FOR 2
+
+let array=["Hola","me","llamo","Juan"];
+for(let elemento of array){
+    console.log(elemento);
+}
+
+//FOR 3
+
+for(let indice in array) {
+    console.log(indice);
+    console.log(array[indice]);
+}
+
+
