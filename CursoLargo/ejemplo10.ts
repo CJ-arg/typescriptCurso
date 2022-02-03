29ea5a99fc234a010926ee6d729ce575f321375a
unknown <janoncarlos@gmail.com>
2022-02-03 13:37:20 -0300

    leccion 10

---
+++ CursoLargo/ejemplo10.ts
@@ -0,0 +1,44 @@
+//SWITCH
+let dia:number=4;
+
+switch(dia)
+{
+    case 1:
+        console.log("Lunes");
+        break;
+    case 2:
+        console.log("Martes");
+        break;
+    case 3:
+        console.log("Miercoles");
+        break;
+    case 4:
+        console.log("Jueves");
+        break;
+    case 5:
+        console.log("Viernes");
+        break;
+    case 6:
+        console.log("Sabado");
+        break;
+    case 7:
+        console.log("Domingo");
+        break;
+    default:
+        console.log("No contemplado");
+        break;
+}
+
+let x = 10, y = 5;
+switch (x-y) {
+    case 0:
+        console.log("0");
+        break;
+    case 5:
+        console.log("5");
+        break;
+    case 10:
+        console.log("10");
+        break;
+}
+
